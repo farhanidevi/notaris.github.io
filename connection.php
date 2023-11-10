00d57c33560c3bdfd2bb451fa383b7d7bf7ccea6
farhanidevi <150350815+farhanidevi@users.noreply.github.com>
2023-11-10 11:10:30 +0700

    Add files via upload

---
+++ connection.php
@@ -0,0 +1,12 @@
+<?php
+	//variabel untuk menampung data koneksi
+	
+	$host 	= "localhost";
+	$user	= "root";
+	$passwd	= "";
+	$dbname	= "db_notaris";
+	
+	$conn = new mysqli($host, $user, $passwd, $dbname);
+
+
+?>
